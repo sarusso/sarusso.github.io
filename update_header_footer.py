f7c59120284d2a34acb0b8c01b1977b9a97505c7
Stefano Alberto Russo <stefano.russo@gmail.com>
2022-01-18 01:37:42 +0100

    Updated website with python assembling scriot and added container blog article.

---
+++ update_header_footer.py
@@ -0,0 +1,49 @@
+import os
+import uuid
+from shutil import copy
+
+# Support vars
+header = ''
+footer = ''
+footer_started = False
+header_ended = False
+
+# First, get the header and footer code from the index.html
+with open('index.html') as f:
+    for line in f.readlines():
+        if not header_ended:
+            if 'phx6a' in line:
+                header_ended = True
+            else:
+                header += line
+        if 'phx6b' in line:
+            footer_started = True
+        if footer_started:
+            footer += line
+
+# Then, create new files for each html file:
+bk_dir = './BK_{}'.format(uuid.uuid4())
+os.mkdir(bk_dir)
+for file_name in os.listdir('.'):
+
+    if file_name.endswith('.html') and file_name != 'index.html':
+        
+        print('Processing {}'.format(file_name))
+        copy(file_name,'{}/{}'.format(bk_dir,file_name))
+        
+        footer_started = False
+        header_ended = False
+        content = '' 
+        
+        with open(file_name) as f:
+            for line in f.readlines():
+                if not header_ended:
+                    if 'phx6a' in line:
+                        header_ended = True
+                if 'phx6b' in line:
+                    footer_started = True
+                if header_ended and not footer_started:
+                    content += line
+        with open(file_name, 'w') as f:
+            f.write(header + content + footer)
+        break
