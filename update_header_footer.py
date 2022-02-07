69354b0f5e6693bf4982ea49e0f4542cb55a4549
Stefano Alberto Russo <stefano.russo@gmail.com>
2022-02-07 13:10:16 +0100

    Made the title persistent when updateing header and footer. Minor fiexs.

--- update_header_footer.py
+++ update_header_footer.py
@@ -6,20 +6,25 @@ from shutil import copy
 header = ''
 footer = ''
 footer_started = False
+header_started = False
 header_ended = False
 
 # First, get the header and footer code from the index.html
 with open('index.html') as f:
     for line in f.readlines():
-        if not header_ended:
-            if 'phx6a' in line:
-                header_ended = True
-            else:
-                header += line
-        if 'phx6b' in line:
-            footer_started = True
-        if footer_started:
-            footer += line
+        if not header_started:
+            if "<title>" in line:
+                header_started = True
+        else:
+            if not header_ended:
+                if 'phx6a' in line:
+                    header_ended = True
+                else:
+                    header += line
+            if 'phx6b' in line:
+                footer_started = True
+            if footer_started:
+                footer += line
 
 # Then, create new files for each html file:
 bk_dir = './BK_{}'.format(uuid.uuid4())
@@ -33,16 +38,34 @@ for file_name in os.listdir('.'):
         
         footer_started = False
         header_ended = False
+        header_started = False
+        pre_header = ''
         content = '' 
         
         with open(file_name) as f:
             for line in f.readlines():
-                if not header_ended:
-                    if 'phx6a' in line:
-                        header_ended = True
-                if 'phx6b' in line:
-                    footer_started = True
-                if header_ended and not footer_started:
-                    content += line
+                
+                if not header_started:
+                    pre_header += line
+                    if "<title>" in line:
+                        header_started = True
+                else:   
+                    if not header_ended:
+                        if 'phx6a' in line:
+                            header_ended = True
+                    if 'phx6b' in line:
+                        footer_started = True
+                    if header_ended and not footer_started:
+                        content += line
         with open(file_name, 'w') as f:
-            f.write(header + content + footer)
+            f.write(pre_header + header + content + footer)
+
+
+
+
+
+
+
+
+
+
