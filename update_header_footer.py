8fb6bca745ce369fe2744e41e97a9a5d8a7ae963
Stefano Alberto Russo <stefano.russo@gmail.com>
2022-10-20 17:14:42 +0200

    Moved to a blog dedicatesd subfolder. Added canoncial version to old blog container ecosystem overivew page.

--- update_header_footer.py
+++ update_header_footer.py
@@ -27,38 +27,49 @@ with open('index.html') as f:
                 footer += line
 
 # Then, create new files for each html file:
-bk_dir = './BK_{}'.format(uuid.uuid4())
-os.mkdir(bk_dir)
-for file_name in os.listdir('.'):
-
-    if file_name.endswith('.html') and file_name != 'index.html':
-        
-        print('Processing {}'.format(file_name))
-        copy(file_name,'{}/{}'.format(bk_dir,file_name))
-        
-        footer_started = False
-        header_ended = False
-        header_started = False
-        pre_header = ''
-        content = '' 
-        
-        with open(file_name) as f:
-            for line in f.readlines():
+root_bk_dir = './BK_{}'.format(uuid.uuid4())
+os.mkdir(root_bk_dir)
+dirs = ['.', 'blog']
+for dir in dirs:
+    if dir != '.':
+        os.mkdir('{}/{}'.format(root_bk_dir, dir))
+    for file_name in os.listdir(dir):
+    
+        if file_name.endswith('.html') and file_name != 'index.html':
+            
+            print('Processing {}/{}'.format(dir,file_name))
+            copy('{}/{}'.format(dir,file_name),'{}/{}/{}'.format(root_bk_dir,dir,file_name))
+            
+            footer_started = False
+            header_ended = False
+            header_started = False
+            pre_header = ''
+            content = '' 
+            
+            with open('{}/{}'.format(dir,file_name)) as f:
+                for line in f.readlines():
+                    
+                    if not header_started:
+                        pre_header += line
+                        if "<title>" in line:
+                            header_started = True
+                    else:   
+                        if not header_ended:
+                            if 'phx6a' in line:
+                                header_ended = True
+                        if 'phx6b' in line:
+                            footer_started = True
+                        if header_ended and not footer_started:
+                            content += line
+            with open('{}/{}'.format(dir,file_name), 'w') as f:
+                if dir != '.':
+                    this_header = header.replace('./', '../')
+                    this_footer = footer.replace('./', '../')
+                else:
+                    this_header = header
+                    this_footer = footer
                 
-                if not header_started:
-                    pre_header += line
-                    if "<title>" in line:
-                        header_started = True
-                else:   
-                    if not header_ended:
-                        if 'phx6a' in line:
-                            header_ended = True
-                    if 'phx6b' in line:
-                        footer_started = True
-                    if header_ended and not footer_started:
-                        content += line
-        with open(file_name, 'w') as f:
-            f.write(pre_header + header + content + footer)
+                f.write(pre_header + this_header + content + this_footer)
 
 
 
