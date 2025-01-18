9475e86656b1ff5cdfef284047316abde4b87629
Stefano Alberto Russo <stefano.russo@gmail.com>
2025-01-18 19:51:16 +0100

    Minor fixes in the update header and footer script.

--- update_header_footer.py
+++ update_header_footer.py
@@ -34,21 +34,21 @@ for dir in dirs:
     if dir != '.':
         os.mkdir('{}/{}'.format(root_bk_dir, dir))
     for file_name in os.listdir(dir):
-    
-        if file_name.endswith('.html') and file_name != 'index.html':
-            
+
+        if file_name.endswith('.html') and file_name not in ['index.html', 'blog_container_engines_runtimes_orchestrators.html']:
+
             print('Processing {}/{}'.format(dir,file_name))
             copy('{}/{}'.format(dir,file_name),'{}/{}/{}'.format(root_bk_dir,dir,file_name))
-            
+
             footer_started = False
             header_ended = False
             header_started = False
             pre_header = ''
             content = '' 
-            
+
             with open('{}/{}'.format(dir,file_name)) as f:
                 for line in f.readlines():
-                    
+
                     if not header_started:
                         pre_header += line
                         if "<title>" in line:
@@ -68,15 +68,6 @@ for dir in dirs:
                 else:
                     this_header = header
                     this_footer = footer
-                
+
                 f.write(pre_header + this_header + content + this_footer)
-
-
-
-
-
-
-
-
-
 
